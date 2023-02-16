599160739d4609e1a6d0a63c117c8b3c23668d66
Jcdcm20 <jcdcm20@gmail.com>
2023-02-16 10:09:59 -0300

    Finish substrings method

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,6 +1,19 @@
-def substrings(word, dic)
+def substrings(string, dic)
+    arr = string.downcase.split()
+    
+    hash = arr.reduce(Hash.new(0)) do |list, value|
+        dic.each do |word|
+            if value.include?(word)
+                list[word] += 1
+            end
+        end
+        list
+    end
 
+    p hash
 end
 
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", 
             "it", "i", "low", "own", "part", "partner", "sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
