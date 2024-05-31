f3176bd672e338a8f3a8c42280118ddb25dbae0c
jcdcm <jcdcm20@gmail.com>
2024-05-31 16:18:58 -0300

    Add gemfile

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -1,37 +1,37 @@
 def caesar_cipher(word, shift)
-    letters = word.split('')
-    temp = []
-    final_word = ''
+  letters = word.split('')
+  temp = []
+  final_word = ''
 
-    letters.each do |l|
-        if l.ord.between?(65, 90)
-            if l.ord + shift > 90
-                temp.push((l.ord + shift) - 26)
-            else
-                temp.push(l.ord + shift)
-            end
-        elsif l.ord.between?(97, 122)
-            if l.ord + shift  >  122
-                temp.push((l.ord + shift) - 26)
-            else
-                temp.push(l.ord + shift)
-            end
-        else
-            temp.push(l.ord)
-        end
+  letters.each do |l|
+    if l.ord.between?(65, 90)
+      if l.ord + shift > 90
+        temp.push((l.ord + shift) - 26)
+      else
+        temp.push(l.ord + shift)
+      end
+    elsif l.ord.between?(97, 122)
+      if l.ord + shift > 122
+        temp.push((l.ord + shift) - 26)
+      else
+        temp.push(l.ord + shift)
+      end
+    else
+      temp.push(l.ord)
     end
-    
-    temp.each do |l|
-        final_word += l.chr
-    end
+  end
 
-    puts final_word
+  temp.each do |l|
+    final_word += l.chr
+  end
+
+  puts final_word
 end
 
-print "Enter a phrase you wish to encrypt: "
+print 'Enter a phrase you wish to encrypt: '
 word = gets
 
-print "Enter the shift you wish to apply: "
+print 'Enter the shift you wish to apply: '
 shift = gets.to_i
 
 caesar_cipher(word, shift)
