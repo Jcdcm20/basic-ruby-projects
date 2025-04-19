55c2f027170abd13f5d681dfa0caeec4ddc3ea29
Júlio César Moreira <jcdcm20@gmail.com>
2025-04-19 09:59:39 -0300

    Fix bundle versions and refactor code

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -2,16 +2,16 @@
 
 def caesar_cipher(phrase, shift)
   encrypted = ''
-  phrase.chars.each do | letter |
+  phrase.chars.each do |letter|
     encrypted += if letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
-                    if letter.ord + shift <= 90 || letter.ord + shift <= 122
-                      (letter.ord + shift).chr
-                    else
-                      (letter.ord + shift - 26).chr
-                    end
-                  else
-                    letter
-                  end
+                   if letter.ord + shift <= 90 || letter.ord + shift <= 122
+                     (letter.ord + shift).chr
+                   else
+                     (letter.ord + shift - 26).chr
+                   end
+                 else
+                   letter
+                 end
   end
   encrypted
 end
