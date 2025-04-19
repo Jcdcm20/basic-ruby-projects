406f2ecaa7cfd0906ad2e634e9e60e7c233499c9
Júlio César Moreira <jcdcm20@gmail.com>
2025-04-19 09:51:46 -0300

    Refactor code

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -1,39 +1,25 @@
 # frozen_string_literal: true
 
-def caesar_cipher(word, shift)
-  letters = word.chars
-  temp = []
-  final_word = ''
-
-  letters.each do |l|
-    if l.ord.between?(65, 90)
-      if l.ord + shift > 90
-        temp.push((l.ord + shift) - 26)
-      else
-        temp.push(l.ord + shift)
-      end
-    elsif l.ord.between?(97, 122)
-      if l.ord + shift > 122
-        temp.push((l.ord + shift) - 26)
-      else
-        temp.push(l.ord + shift)
-      end
-    else
-      temp.push(l.ord)
-    end
+def caesar_cipher(phrase, shift)
+  encrypted = ''
+  phrase.chars.each do | letter |
+    encrypted += if letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
+                    if letter.ord + shift <= 90 || letter.ord + shift <= 122
+                      (letter.ord + shift).chr
+                    else
+                      (letter.ord + shift - 26).chr
+                    end
+                  else
+                    letter
+                  end
   end
-
-  temp.each do |l|
-    final_word += l.chr
-  end
-
-  puts final_word
+  encrypted
 end
 
 print 'Enter a phrase you wish to encrypt: '
-word = gets
+word = gets.chomp
 
 print 'Enter the shift you wish to apply: '
 shift = gets.to_i
 
-caesar_cipher(word, shift)
+puts caesar_cipher(word, shift)
